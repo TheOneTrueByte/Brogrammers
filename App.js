be00acc815023daeac85d6d9ce6958fe73459fb3
nhandang9594 <tamnhan.dang9594@gmail.com>
2021-10-25 17:55:38 -0500

    add native base

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
