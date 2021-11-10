491b1f709dd95773f16998941e73f81021e31983
nhandang9594 <tamnhan.dang9594@gmail.com>
2021-11-09 21:41:21 -0600

    adding flatlist for store screen

--- StoreSelection.js
+++ StoreSelection.js
@@ -1,36 +1,186 @@
-import * as React from "react";
-import { Button, View, Text } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import React, { useState } from "react";
+import {
+  Button,
+  View,
+  Text,
+  Alert,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import { createDrawerNavigator, DrawerItem } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import Login from "./Login";
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button
-        onPress={() => navigation.navigate("Notifications")}
-        title="Go to notifications"
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+function Stores() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    const color = item.id === selectedId ? "white" : "black";
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{ backgroundColor }}
+        textColor={{ color }}
       />
+    );
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex start",
+        alignItems: "center",
+      }}
+    >
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+        />
+        <Button
+          title="Press me"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
+      </SafeAreaView>
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function TeamAndStorage() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Team and Storage screen</Text>
+    </View>
+  );
+}
+
+function Inventory() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Inventory</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Setting Screen</Text>
+    </View>
+  );
+}
+
+function LogOut() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>LogOut Screen</Text>
+    </View>
+  );
+}
+
+function About() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>About Screen</Text>
     </View>
   );
 }
 
 const Drawer = createDrawerNavigator();
 
-function StoreSelection() {
+function MyDrawer() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+    <Drawer.Navigator
+      initialRouteName="Stores"
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "red",
+        },
+        drawerLabelStyle: {
+          color: "white",
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Stores"
+        component={Stores}
+        options={{ drawerLabel: "Stores" }}
+      />
+      <Drawer.Screen
+        name="TeamAndStorage"
+        component={TeamAndStorage}
+        options={{ drawerLabel: "TeamAndStorage" }}
+      />
+      <Drawer.Screen
+        name="Inventory"
+        component={Inventory}
+        options={{ drawerLabel: "Inventory" }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={Settings}
+        options={{ drawerLabel: "Settings" }}
+      />
+      <Drawer.Screen
+        name="LogOut"
+        component={LogOut}
+        options={{ drawerLabel: "LogOut" }}
+      />
+      <Drawer.Screen
+        name="About"
+        component={About}
+        options={{ drawerLabel: "About" }}
+      />
     </Drawer.Navigator>
   );
 }
+
+function StoreSelection() {
+  return <MyDrawer />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 20,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default StoreSelection;
