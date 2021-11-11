68d736cb0d70461b2ae8b1cd2a00a4235524f320
Nhan Dang <tamnhan.dang9594@gmail.com>
2021-11-11 12:14:39 -0600

    Adjust the add button for the flat list

--- StoreSelection.js
+++ StoreSelection.js
@@ -13,65 +13,44 @@ import {
 import { createDrawerNavigator, DrawerItem } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./Login";
+import { style } from "styled-system";
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Item",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Item",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Item",
-  },
-];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>{item.title}</Text>
-  </TouchableOpacity>
-);
 
 function Stores() {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
-    const color = item.id === selectedId ? "white" : "black";
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
-    );
-  };
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "flex start",
-        alignItems: "center",
-      }}
-    >
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          extraData={selectedId}
-        />
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert("Simple Button pressed")}
-        />
-      </SafeAreaView>
+  const DATA = [
+    {
+      id: '1',
+      title: 'Store 1',
+    },
+    {
+      id: '2',
+      title: 'Store 2',
+    },
+    {
+      id: '3',
+      title: 'Store 3',
+    }
+  ]
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
     </View>
+  );
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -170,13 +149,18 @@ function StoreSelection() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 20,
+
+    paddingTop: 40,
+    paddingHorizontal: 20,
+
   },
   item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   },
   title: {
     fontSize: 32,
