add7cbff581f31c90070eaf94f9c4273fac79617
jvguzman21 <86018447+jvguzman21@users.noreply.github.com>
2022-03-10 23:16:52 -0600

    deleted create, added addTeams screen

--- components/AddItem.js
+++ components/AddItem.js
@@ -6,5 +6,17 @@ import {
   Button,
   SafeAreaView,
   Alert,
+  View,
+  Text,
 } from "react-native";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
+//this will essentially be a form 
+//that will prompt the user for
+//the name, color, and quantity
+//of a new item
+export default function AddTeamItem({ navigation }) {
+  return (
+    <Text>This is where new items are created</Text>
+  );
+}
--- components/items.js
+++ components/items.js
@@ -24,13 +24,14 @@ import {
 } from "react-native";
 import { useState } from "react";
 import { createDrawerNavigator, DrawerItem } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { TextInput } from "react-native-gesture-handler";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./Login";
 
 import { background, borderLeft, style } from "styled-system";
 import AddItem from "./AddTeam";
+import AddTeamItem from "./AddItem";
 
 import { initializeApp } from "@firebase/app";
 import "firebase/firestore";
@@ -38,11 +39,75 @@ import { getFirestore, setDoc, doc, deleteDoc } from 'firebase/firestore';
 import { Icon } from "native-base";
 import { getAuth, updateEmail, signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { ScreenStack } from "react-native-screens";
 
-export default function ViewItems({ navigation }) {
+const ItemsStack = createNativeStackNavigator();
+
+function AddItemsScreen({ navigation }) {
+  return <AddTeamItem />
+}
+
+
+function ViewItems({ navigation }) {
     const [teamItems, setTeamItems] = useState("");
 
     return (
-        <Text>this is where the teams's items will be</Text>
-    )
+        <SafeAreaView style = { styles.container }>
+            <View style = {styles.addItemView}>
+              <Pressable 
+                style = {styles.addItemButton}
+                onPress={() => navigation.navigate("AddItemsScreen")}
+              >
+                <Text style = {styles.addItemButtonText} >Add Item</Text>
+              </Pressable> 
+            </View>
+          <Text>this is where the teams's items will be</Text>
+        </SafeAreaView>
+    );
 }
+
+export default function ItemsNavigator({ navigation })
+{
+  return (
+    <ItemsStack.Navigator 
+      initialRouteName="ViewItems"
+      //screenOptions={{ gestureEnabled: false }}
+    >
+      <ItemsStack.Screen 
+        name = "ViewItems"
+        component = {ViewItems}
+        options = {{ headerShown: false, }}
+      />
+      <ItemsStack.Screen 
+        name = "AddItemsScreen"
+        component = {AddItemsScreen}
+        options = {{ headerShown: false, }}
+      />
+    </ItemsStack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+  },
+  addItemView: {
+    padding: 8,
+    borderRadius: 8,
+  },
+  addItemButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#ff4545",
+    marginBottom: 8,
+    padding: 10,
+    borderRadius: 8,
+    justifyContent: "center",
+  },
+  addItemButtonText: {
+    fontSize: 16,
+    color: "white",
+  }
+})
